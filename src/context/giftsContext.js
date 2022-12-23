4eb17ec0c869fdf21a55bbd5ab476a45bf57f56d
Alejo Lang <alejolang123@gmail.com>
2022-12-23 12:22:06 -0300

    Add context for the gift list

---
+++ src/context/giftsContext.js
@@ -0,0 +1,14 @@
+const { createContext, useState } = require("react");
+
+export const giftsContext = createContext
+
+export function GiftsContextProvider (props) {
+
+    const [giftsList, updateGiftsList] = useState([]);
+
+    return (
+        <giftsContext.Provider value={{giftsList, updateGiftsList}}>
+            {props.children}
+        </giftsContext.Provider>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GiftsContextProvider } from 'context/giftsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GiftsContextProvider>
+      <App />
+    </GiftsContextProvider>
   </React.StrictMode>
 );
