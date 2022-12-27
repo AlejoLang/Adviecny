c9674868b2f94817ad437d5fb3a652d1101255e5
Alejo Lang <alejolang123@gmail.com>
2022-12-26 22:21:27 -0300

    Add functions for open form and clear items

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import GiftList from 'components/GiftsList';
 import { useContext } from 'react';
 import { giftsContext } from 'context/giftsContext';
 import './App.css';
+import GiftForm from 'components/GiftForm';
 
 function App() {
 
   const {updateGiftsList} = useContext(giftsContext)
 
-  const clearList = () => {
+  const openForm = () => {
+    const dialog = document.querySelector('dialog')
+    if(dialog){dialog.open = true}
+  }
+
+  const clearList = (e) => {
+    e.preventDefault()
     updateGiftsList([])
   }
 
   return (
     <div className="App">
-      <div className='giftsDiv'> 
+      <div className='giftsDiv'>
         <h1 className='mainTitle'>Regalos</h1>
-        <button className='openFormBtn'>Agregar Regalo</button>
+        <button className='openFormBtn' onClick={openForm}>Agregar Regalo</button>
         <GiftList />
         <button className="clearListBtn" onClick={clearList}>Borrar todo</button>
       </div>
+      <GiftForm />
     </div>
   );
 }
