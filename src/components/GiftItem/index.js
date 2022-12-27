6bc0793ab1bd807999c867c37bb9cd5ef6b598b8
Alejo Lang <alejolang123@gmail.com>
2022-12-26 22:19:38 -0300

    Create Gift component and add basic styles

--- src/components/GiftItem/index.js
+++ src/components/GiftItem/index.js
@@ -1,6 +1,22 @@
+import { useContext } from 'react'
+import { giftsContext } from 'context/giftsContext'
+import './styles.css'
+
 function GiftItem({itemData}) {
+  console.log(itemData)
   return (
-    <div>{itemData}</div>
+    <li className="giftItem">
+      <img src={itemData.img} alt={itemData.nombre} className='giftImg'/>
+      <div className="giftMainInfo">
+        <p className="giftName">{itemData.nombre} ({itemData.cant})</p>
+        <p className="giftDest">{itemData.dest}</p>
+      </div>
+      <p className="giftPrice">
+        {itemData.precio * itemData.cant}
+      </p>
+      <button className="editGiftBtn">E</button>
+      <button className="deleteGiftBtn">X</button>
+    </li>
   )
 }
 
