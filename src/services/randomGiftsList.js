13b5defb2d9148241031b83be0c135f21612d9d2
Alejo Lang <alejolang123@gmail.com>
2022-12-26 23:24:49 -0300

    Add a list of random gifts

---
+++ src/services/randomGiftsList.js
@@ -0,0 +1,37 @@
+export const randomGiftsList = [
+    {
+        nombre: 'Medias x3',
+        precio: 800,
+        cant: 1,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_780775-MLA52491297555_112022-W.webp'
+    },
+    {
+        nombre: 'Mouse Logitech G305',
+        precio: 10000,
+        cant: 1,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_977588-MLA51172463642_082022-V.webp'
+    },
+    {
+        nombre: 'Termo Lumilagro',
+        precio: 11900,
+        cant: 1,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_934089-MLA42441068436_072020-V.webp'
+    },
+    {
+        nombre: 'Cubo Rubik 3x3 Warrior W',
+        precio: 1649,
+        cant: 1,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_730276-MLA40147249639_122019-V.webp'
+    },
+    {
+        nombre: 'Auriculares Xiaomi Redmi AirDots',
+        precio: 5200,
+        cant: 1,
+        img: 'https://http2.mlstatic.com/D_NQ_NP_656991-MLA40756758814_022020-V.webp'
+    },
+    {
+        nombre: 'Linterna Spica x40',
+        precio: 10000,
+        cant: 1
+    },
+]
