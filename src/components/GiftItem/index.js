69fe4d71d1117f5c920fa64367c5fae3cab51a26
Alejo Lang <alejolang123@gmail.com>
2022-12-26 22:49:00 -0300

    Add delete function for the gift tiem, modify some styles

--- src/components/GiftItem/index.js
+++ src/components/GiftItem/index.js
@@ -3,19 +3,31 @@ import { giftsContext } from 'context/giftsContext'
 import './styles.css'
 
 function GiftItem({itemData}) {
-  console.log(itemData)
+  
+  const {giftsList, updateGiftsList} = useContext(giftsContext)
+
+  const handleItemDelete = () => {
+    updateGiftsList(
+      giftsList.filter(item => item.id !== itemData.id)
+    )
+  }
+
   return (
     <li className="giftItem">
-      <img src={itemData.img} alt={itemData.nombre} className='giftImg'/>
+      {
+        itemData.img ? <img src={itemData.img} alt={itemData.nombre} className='giftImg'/> : ''
+      }
       <div className="giftMainInfo">
         <p className="giftName">{itemData.nombre} ({itemData.cant})</p>
         <p className="giftDest">{itemData.dest}</p>
       </div>
       <p className="giftPrice">
-        {itemData.precio * itemData.cant}
+        $ {itemData.precio * itemData.cant}
       </p>
-      <button className="editGiftBtn">E</button>
-      <button className="deleteGiftBtn">X</button>
+      <div className="giftButtons">
+        <button className="editGiftBtn">E</button>
+        <button className="deleteGiftBtn" onClick={handleItemDelete}>X</button>
+      </div>
     </li>
   )
 }
