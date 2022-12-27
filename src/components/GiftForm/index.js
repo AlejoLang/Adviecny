2f9e6246435c7490d4ba43977f6c441d7eb5e8b7
Alejo Lang <alejolang123@gmail.com>
2022-12-26 23:24:30 -0300

    Add 'get random item button'

--- src/components/GiftForm/index.js
+++ src/components/GiftForm/index.js
@@ -1,5 +1,6 @@
 import { giftsContext } from 'context/giftsContext'
 import { useContext } from 'react'
+import { randomGiftsList } from 'services/randomGiftsList'
 import './styles.css'
 
 function GiftForm() {
@@ -11,10 +12,10 @@ function GiftForm() {
 
         const newGift = {
             nombre: e.target[0].value,
-            precio: e.target[1].value,
-            dest: e.target[2].value ?? '',
-            img: e.target[3].value ?? '',
-            cant: e.target[4].value,
+            precio: e.target[2].value,
+            cant: e.target[3].value,
+            dest: e.target[4].value ?? '',
+            img: e.target[5].value ?? '',
             id: Math.random()
         }
 
@@ -30,14 +31,34 @@ function GiftForm() {
         document.querySelector('form').reset()
     }
 
+    const getRandomItem = (e) => {
+        e.preventDefault()
+
+        const form = document.querySelector('.addGiftForm')
+
+        let selectedGift = {}
+        selectedGift.nombre = form[0].value
+
+        while(selectedGift.nombre === form[0].value){
+            console.log(selectedGift)
+            selectedGift = randomGiftsList[Math.floor(Math.random() * randomGiftsList.length)]
+        }
+
+        form[0].value = selectedGift.nombre
+        form[2].value = selectedGift.precio
+        form[3].value = selectedGift.cant
+        form[5].value = selectedGift.img
+    }
+
   return (
     <dialog className='formModal'>
         <form onSubmit={handleFormSubmit} className='addGiftForm'>
             <input type="text" placeholder='Regalo' required className='formInput giftNameInput'/>
+            <button className="getRandomItemBtn" onClick={getRandomItem}>Sorprendeme</button>
             <input type="number" placeholder='Precio' required className='formInput giftPriceInput'/>
+            <input type="number" placeholder='Cantidad' required className='formInput giftAmmInput'/>
             <input type="text" placeholder='Destinatario' className='formInput'/>
             <input type="text" placeholder='Link a imagen' className='formInput'/>
-            <input type="number" placeholder='Cantidad' required className='formInput'/>
             <button onClick={handleFormClose} className='closeFormBtn'>Cerrar</button>
             <button className='addGiftBtn'>Guardar</button>
         </form>
